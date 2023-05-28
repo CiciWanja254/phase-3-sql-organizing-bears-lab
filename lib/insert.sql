8b84a5df1d99b65c798e2c14a3253dbff027aa62
Cici254 <thukucindy00@gmail.com>
2023-05-28 18:21:08 +0300

    Complete assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex) VALUES ('Mr. Chocolate', 5, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Rowdy', 3, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Tabitha', 4, 'F');
+INSERT INTO bears (name, age, sex) VALUES ('Sergeant Brown', 7, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Melissa', 6, 'F');
+INSERT INTO bears (name, age, sex) VALUES ('Grinch', 2, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Wendy', 8, 'F');
+INSERT INTO bears (name, age, sex) VALUES (NULL, 23, 'F');
